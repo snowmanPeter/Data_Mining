b9b57476e84d5d751087e3e086271c53b1c1e3cc
master <zjutedu1001@163.com>
2017-07-23 19:43:12 +0800

    BIRCH模型构建

---
+++ Clustering/BIRCH.py
@@ -0,0 +1,17 @@
+#encoding:utf-8
+
+"""
+    BIRCH模型的核心是CF模型，下面将主要构建CF模型
+"""
+
+class BTNode():
+
+
+class CFNode():
+
+    def __init__(self,n,LS,SS):
+        self.n = n
+        self.SL = LS
+        self.SS = SS
+
+class LeafNode():
