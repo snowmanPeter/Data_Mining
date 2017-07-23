1bec33b31ccce2e0080e3007d310c4243e36b689
master <zjutedu1001@163.com>
2017-07-23 19:47:53 +0800

    BIRCH模型构建

--- Clustering/BIRCH.py
+++ Clustering/BIRCH.py
@@ -6,12 +6,15 @@
 
 class BTNode():
 
+    def __init__(self,cfNum,cfNode):
+        self.cfNum= cfNum
+        self.cfNode = cfNode
 
 class CFNode():
 
-    def __init__(self,n,LS,SS):
+    def __init__(self,n,LS,SS,btNode):
         self.n = n
         self.SL = LS
         self.SS = SS
+        self.btNOde = btNode
 
-class LeafNode():
